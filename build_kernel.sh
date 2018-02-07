e01993d147e7bb53ba20f8ecf8f9ee14372e208d
kongwoojin <kongwoojin@naver.com>
2018-02-07 21:02:04 +0900

    build_kernel.sh: Use AnyKernel2 to make flashable zip
    AnyKernel2 from https://github.com/osm0sis/AnyKernel2
    
    Signed-off-by: kongwoojin <kongwoojin@naver.com>

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
+# Kernel Build Script by Kongjak
 
 export ARCH=arm
 export CROSS_COMPILE=/opt/toolchains/arm-eabi-4.7/bin/arm-eabi-
 mkdir output
 
-make -C $(pwd) O=output msm8916_sec_defconfig VARIANT_DEFCONFIG=msm8916_sec_grandmax_koropen_defconfig SELINUX_DEFCONFIG=selinux_defconfig
-make -C $(pwd) O=output
+echo "Start build kernel..."
 
-cp output/arch/arm/boot/Image $(pwd)/arch/arm/boot/zImage
+make -C $(pwd) O=output msm8916_sec_defconfig VARIANT_DEFCONFIG=msm8916_sec_grandmax_koropen_defconfig SELINUX_DEFCONFIG=selinux_defconfig
+make -j16 -C $(pwd) O=output
+
+cp $(pwd)/output/arch/arm/boot/zImage $(pwd)/utilities/AnyKernel2/zImage
+
+echo "Strip Prima wlan module..."
+${CROSS_COMPILE}strip --strip-unneeded $(pwd)/output/drivers/staging/prima/wlan.ko
+cp $(pwd)/output/drivers/staging/prima/wlan.ko $(pwd)/utilities/AnyKernel2/modules/system/lib/modules/pronto/pronto_wlan.ko
+
+echo "Make flashable zip..."
+cd $(pwd)/utilities/AnyKernel2
+./zip.sh
+cd ../../
+
+cp $(pwd)/utilities/AnyKernel2/Kernel.zip .
+echo "Finish!"
