d1d846059261df303992646e027a183dcad0cade
kongwoojin <kongwoojin@naver.com>
2018-02-18 17:40:40 +0900

    clean_kernel.sh: Add clean_kernel.sh script
    Signed-off-by: kongwoojin <kongwoojin@naver.com>

---
+++ clean_kernel.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Kernel Build Script by Kongjak
+
+export ARCH=arm
+export CROSS_COMPILE=/opt/toolchains/arm-eabi-4.7/bin/arm-eabi-
+
+echo "Start cleaning kernel..."
+
+cd output
+make clean && make mrproper
+cd ../
+
+echo "Remove output directory..."
+rm -rf output
+
+echo "Remove output files..."
+rm -rf utilities/AnyKernel2/Kernel.zip Kernel.zip utilities/AnyKernel2/zImage utilities/AnyKernel2/modules/system/lib/modules/pronto/pronto_wlan.ko arch/arm/boot/zImage
+
+echo "Finish!"
